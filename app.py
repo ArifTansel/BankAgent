c68aed7f7b92ddef0b3115bccb3c648d209630cb
ArifTansel <ariftansel8@gmail.com>
2024-10-31 12:39:34 +0300

    Login and logout

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for, request
 from flask_mysql_connector import MySQL
-from werkzeug.security import generate_password_hash ,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 import requests
 import json
 app = Flask(__name__)
@@ -25,37 +25,41 @@ def index():
 @app.route("/signup", methods=['POST'])
 def signUp():
     session["is_used"] = False
-    cursor = mysql.connection.cursor()
-    username = request.form.get("username").strip().casefold()
-    mail = request.form.get("email").strip()
-    password = request.form.get("password").strip()
-    hashed_password = generate_password_hash(password)
-    cursor.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)",
-                    (username, mail, hashed_password))
-    mysql.connection.commit()
-    cursor.close()
-    session["is_used"] = False
+    try:
+        cursor = mysql.connection.cursor()
+        username = request.form.get("username").strip().casefold()
+        mail = request.form.get("email").strip()
+        password = request.form.get("password").strip()
+        hashed_password = generate_password_hash(password)
+        cursor.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)",
+                       (username, mail, hashed_password))
+        mysql.connection.commit()
+        cursor.close()
+        session["is_used"] = False
 
-    # except:
-    #     session["is_used"] = True
-    return redirect(url_for('index'))
+    except:
+        session["is_used"] = True
+        return redirect(url_for('index'))
 
 
 @app.route("/login", methods=['POST'])
 def login():
-    try :
+    try:
         cursor = mysql.connection.cursor()
         username = request.form.get("username").strip().casefold()
         password = request.form.get("password").strip()
-        cursor.execute("SELECT password FROM users WHERE username=%s", (username,))
+        cursor.execute(
+            "SELECT password FROM users WHERE username=(%s)", (username,))
         result = cursor.fetchone()
         cursor.close()
         if result is None:  # kullanıcı adı bulunamadı
             session["is_wrong"] = True
             return redirect(url_for("index"))
 
-        if check_password_hash(result[0],password):
-            print("şifre doğru :" , username)
+        if check_password_hash(result[0], password):
+            print("şifre doğru :", username)
+            session["is_used"] = False
+            session["is_wrong"] = False
             session["username"] = username
             return redirect(url_for("index"))
 
@@ -67,24 +71,26 @@ def login():
         print("hata")
         session["is_error"] = True
         return redirect(url_for("index"))
-@app.route("/send_message",methods=["POST"])
-def sendMessage() : 
+
+
+@app.route("/send_message", methods=["POST"])
+def sendMessage():
     prompt = request.get_json()
-    message = prompt["message"] 
+    message = prompt["message"]
     data = {
         "model": "llama3.1:8b",
         "prompt": message,
         "stream": False
     }
     response = requests.post("http://localhost:11434/api/generate", json=data)
-    rp  = json.dumps(response.text)
+    rp = json.dumps(response.text)
     return rp
+
+@app.route("/logout")
+def logout():
+    session["username"] = None 
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
-
-# cursor = mysql.connection.cursor()
-# cursor.execute("INSERT INTO users (username,email,password) VALUES ('asdasd','blabla','patates')")
-# mysql.connection.commit()
-# cursor.close()
